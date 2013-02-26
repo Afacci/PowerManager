2ceeef7046e207a0cad9494fee5df0002340b187
andrea <andrea@ueuexLaptop>
2013-02-26 11:50:40 +0100

    added shared.f90

---
+++ src/shared.f90
@@ -0,0 +1,45 @@
+!---------------------------------------------------------------------------
+!  
+!   PowerManager.  
+!
+!---------------------------------------------------------------------------
+!License
+!    This file is part of PowerManager.
+!
+!    PowerManager is free software; you can redistribute it and/or modify it
+!    under the terms of the GNU General Public License as published by the
+!    Free Software Foundation; either version 2 of the License, or (at your
+!    option) any later version.
+!
+!    PowerManager is distributed in the hope that it will be useful, but WITHOUT
+!    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+!    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+!    for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with PowerManager; if not, write to the Free Software Foundation,
+!    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+!
+!>\file 
+!>\brief File  prototype. 
+!>\details this is the prototype for all the files of the PowerManger project.
+!> Copy, rename, and modify this this file to create a new procedure or module.
+!>\Author 
+!>     Andrea Facci.
+!
+!---------------------------------------------------------------------------
+
+module shared
+
+!---Declare Module usage---
+
+implicit none
+
+integer, parameter :: prec = kind(1.0)
+real(kind = prec), parameter :: rVal = 1.0
+real(kind = prec), parameter :: zero = 0.0
+
+
+!---Declare Local Variables---
+
+end module shared
